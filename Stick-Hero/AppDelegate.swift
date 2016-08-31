52a7aa1fc5bd2a1f5bec24787b1b906b9a9654d8
lwiosbystep <1286594907@qq.com>
2016-08-31 12:28:07 +0800

    一直想做的游戏
    很不错的学习

---
+++ Stick-Hero/AppDelegate.swift
@@ -0,0 +1,21 @@
+
+///  没事学习下大神怎么玩这个的
+
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+   
+
+}
+
